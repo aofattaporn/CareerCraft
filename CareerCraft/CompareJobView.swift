83f195b5fdf28cf685f52dda5a16fc92fb6977c7
aofattaporn <aofattapon321@gmail.com>
2024-04-17 17:24:06 +0700

    main-page: add base content on main-view

---
+++ CareerCraft/CompareJobView.swift
@@ -0,0 +1,18 @@
+//
+//  CompareJobView.swift
+//  CareerCraft
+//
+//  Created by attaporn on 4/17/24.
+//
+
+import SwiftUI
+
+struct CompareJobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompareJobView()
+}
