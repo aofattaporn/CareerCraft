50aa61e1d6b43d5a9c0ca6183c8ef6e0845689f8
aofattaporn <aofattapon321@gmail.com>
2024-04-20 12:13:43 +0700

    compare-job: select label success

--- CareerCraft/CompareJob/CompareJobView.swift
+++ CareerCraft/CompareJob/CompareJobView.swift
@@ -6,16 +6,76 @@
 //
 
 import SwiftUI
+import SwiftData
+
+private var mockItems: [Job] = [
+    Job(company: "Tech Solutions Inc.", department: "Engineering", salaryRange: "$50,000 - $70,000", location: "New York", workStyle: .onsite, workTime: .fixed, hasbonusFrequency: true, hasSocialSecurity: true, hasProvidentFund: true, hasEquipment: true),
+    Job(company: "Marketing Innovations Co.", department: "Marketing", salaryRange: "$40,000 - $60,000", location: "San Francisco", workStyle: .hybrid, workTime: .flexible, hasbonusFrequency: false, hasSocialSecurity: true, hasProvidentFund: true, hasEquipment: true),
+    Job(company: "DesignWorks Studio", department: "Design", salaryRange: "$60,000 - $80,000", location: "Los Angeles", workStyle: .online, workTime: .flexible, hasbonusFrequency: true, hasSocialSecurity: true, hasProvidentFund: true, hasEquipment: true),
+    Job(company: "Financial Services LLC", department: "Finance", salaryRange: "$70,000 - $90,000", location: "Chicago", workStyle: .onsite, workTime: .fixed, hasbonusFrequency: false, hasSocialSecurity: true, hasProvidentFund: true, hasEquipment: true),
+    Job(company: "Sales Solutions Group", department: "Sales", salaryRange: "$50,000 - $70,000", location: "Seattle", workStyle: .hybrid, workTime: .flexible, hasbonusFrequency: true, hasSocialSecurity: true, hasProvidentFund: true, hasEquipment: true),
+    Job(company: "Human Capital Partners", department: "Human Resources", salaryRange: "$45,000 - $65,000", location: "Boston", workStyle: .online, workTime: .flexible, hasbonusFrequency: false, hasSocialSecurity: true, hasProvidentFund: true, hasEquipment: true)
+]
 
 struct CompareJobView: View {
     
+
     @State private var showSelectLabels = false
     
     @State var company1: Job?
     @State var company2: Job?
+    
     @State private var selectorNum1: Int = 1
     @State private var selectorNum2: Int = 2
     
+    @Query private var jobs: [Job]
+    @State private var jobTypeKeys: [String] = [
+        "company",
+        "department",
+        "salaryRange",
+        "location",
+        "workStyle",
+        "workTime",
+        "hasbonusFrequency",
+        "hasSocialSecurity",
+        "hasProvidentFund",
+        "hasEquipment"
+    ]
+    @State private var filterJobTypeKeys: [String] = [
+        "company",
+        "department",
+        "salaryRange",
+        "location",
+        "workStyle",
+        "workTime",
+        "hasbonusFrequency",
+        "hasSocialSecurity",
+        "hasProvidentFund",
+        "hasEquipment"
+    ]
+
+
+    
+    func value(forKey key: String, company: Job?) -> String {
+        guard let company = company else {
+            return "-"
+        }
+        switch key {
+        case "company": return company.company
+        case "department": return company.department ?? "-"
+        case "salaryRange": return company.salaryRange ?? "-"
+        case "location": return company.location ?? "-"
+        case "workStyle": return company.workStyle.rawValue
+        case "workTime": return company.workTime.rawValue
+        case "hasbonusFrequency": return company.hasbonusFrequency ? "Bonus" : "No Bonus"
+        case "hasSocialSecurity": return company.hasSocialSecurity ? "SocialSecurity" : "No SocialSecurity"
+        case "hasProvidentFund": return company.hasProvidentFund ? "ProvidentFund" : "No ProvidentFund"
+        case "hasEquipment": return company.hasEquipment ? "Equipment" : "No Equipment"
+        default: return "-"
+        }
+    }
+
+
     var body: some View {
         
             VStack(spacing: 12){ // open-vstack
@@ -31,7 +91,7 @@ struct CompareJobView: View {
                             .foregroundColor(Color("secondary"))
                     }.sheet(isPresented: $showSelectLabels) {
                         if #available(iOS 16.0, *) {
-                            SelectLabels()
+                            SelectLabels(jobTypeKeys: self.$jobTypeKeys, filterJobTypeKeys: self.$filterJobTypeKeys)
                                 .presentationDetents([.fraction(0.25), .fraction(0.75)])
                                
                         }
@@ -43,44 +103,78 @@ struct CompareJobView: View {
 
                 
                 // *** show compare ***
-                HStack {
-                    
-                    // *** company-1 ***
-                    NavigationLink(destination: SelectCompany(
-                        company1: $company1,
-                        company2: $company2,
-                        selectorNum: $selectorNum1
-                    )){
-                        VStack {
-                            Text(company1 != nil ? company1!.company : "-")
+                VStack {
+                    HStack {
+                        
+                        // *** company-1 ***
+                        NavigationLink(destination: SelectCompany(
+                            company1: $company1,
+                            company2: $company2,
+                            selectorNum: $selectorNum1
+                        )){
+                            VStack {
+                                Text(company1?.company ?? "-")
+                                    .lineLimit(1)
+                                    .truncationMode(.tail)
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(company1 != nil ? Color("secondary") : Color("bg-grey"))
+                            .foregroundColor(company1 != nil ? .white : .gray)
+                            .cornerRadius(10)
                         }
-                        .padding(.vertical)
-                        .frame(maxWidth: .infinity)
-                        .background(company1 != nil ? Color("secondary") : Color("bg-grey"))
-                        .foregroundColor(company1 != nil ? .white : .gray)
-                        .cornerRadius(10)
-                    }
-  
-                    
-                    
-                    // *** company-2 ***
-                    NavigationLink(destination: SelectCompany(
-                        company1: $company1,
-                        company2: $company2,
-                        selectorNum: $selectorNum2
-                    )){
-                        VStack {
-                            Text(company2 != nil ? company2!.company : "-")
+      
+                        
+                        
+                        // *** company-2 ***
+                        NavigationLink(destination: SelectCompany(
+                            company1: $company1,
+                            company2: $company2,
+                            selectorNum: $selectorNum2
+                        )){
+                            VStack {
+                                Text(company2?.company ?? "-")
+                                    .lineLimit(1)
+                                    .truncationMode(.tail)
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(company1 != nil ? Color("secondary") : Color("bg-grey"))
+                            .foregroundColor(company1 != nil ? .white : .gray)
+                            .cornerRadius(10)
                         }
-                        .padding(.vertical)
-                        .frame(maxWidth: .infinity)
-                        .background(company2 != nil ? Color("secondary") : Color("bg-grey"))
-                        .foregroundColor(company2 != nil ? .white : .gray)
-                        .cornerRadius(10)
-                    }
 
+                    }
                     
-                }
+                    
+                    ScrollView([.vertical]) {
+                        VStack(alignment: .center) {
+                            ForEach(Array(filterJobTypeKeys.enumerated()), id: \.element) { index, key in
+                                HStack {
+                                    VStack {
+                                        Spacer()
+                                        Text(self.value(forKey: key, company: self.company1))
+                                        Spacer()
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .background(index.isMultiple(of: 2) ? Color("bg-grey") : Color.white) // Alternating row colors
+                                    
+                                    VStack {
+                                        Spacer()
+                                        Text(self.value(forKey: key, company: self.company2))
+                                        Spacer()
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .background(index.isMultiple(of: 2) ? Color("bg-grey") : Color.white) // Alternating row colors
+                                }
+                            }
+                        }
+                    }
+                    .defaultScrollAnchor(.top)
+
+
+
+                } // close-vstack
                 
                 Spacer()
                 
--- CareerCraft/CompareJob/SelectLabels.swift
+++ CareerCraft/CompareJob/SelectLabels.swift
@@ -11,12 +11,103 @@ import SwiftData
 
 struct SelectLabels: View {
     
+    @Binding var jobTypeKeys: [String]
+    @Binding var filterJobTypeKeys: [String]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            GeometryReader { geometry in
+                self.generateContent(in: geometry)
+            }
+        }
+        .padding(.all)
+    }
+    
+    private func generateContent(in g: GeometryProxy) -> some View {
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+
+        return ZStack(alignment: .topLeading) {
+            ForEach(self.jobTypeKeys, id: \.self) { platform in
+                self.item(for: platform)
+                    .padding([.horizontal, .vertical], 4)
+                    .alignmentGuide(.leading, computeValue: { d in
+                        if (abs(width - d.width) > g.size.width)
+                        {
+                            width = 0
+                            height -= d.height
+                        }
+                        let result = width
+                        if platform == self.jobTypeKeys.last! {
+                            width = 0 //last item
+                        } else {
+                            width -= d.width
+                        }
+                        return result
+                    })
+                    .alignmentGuide(.top, computeValue: {d in
+                        let result = height
+                        if platform == self.jobTypeKeys.last! {
+                            height = 0 // last item
+                        }
+                        return result
+                    })
+            }
+        }
     }
 
+    func item(for text: String) -> some View {
+        Button(action: {
+            // Toggle filter
+            if let index = filterJobTypeKeys.firstIndex(of: text) {
+                filterJobTypeKeys.remove(at: index)
+            } else {
+                filterJobTypeKeys.append(text)
+            }
+        }, label: {
+            Text(text)
+                .padding(.all, 5)
+                .font(.body)
+                .background(filterJobTypeKeys.contains(text) ? Color("secondary") : Color("bg-grey"))
+                .foregroundColor(filterJobTypeKeys.contains(text) ? .white : .gray)
+                .cornerRadius(5)
+        })
+    }
 }
 
+
+
+
+
 #Preview {
-    SelectLabels()
+    SelectLabels(jobTypeKeys: .constant([
+        "company",
+        "department",
+        "salaryRange",
+        "location",
+        "workStyle",
+        "workTime",
+        "hasbonusFrequency",
+        "hasSocialSecurity",
+        "hasProvidentFund",
+        "hasEquipment"
+    ]),
+    
+    filterJobTypeKeys: .constant([
+        "company",
+        "department",
+        "salaryRange",
+        "location",
+        "workStyle",
+        "workTime",
+        "hasbonusFrequency",
+        "hasSocialSecurity"
+    ]))
 }
